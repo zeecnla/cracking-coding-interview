1b8df42f94731e4eb37da0ffe116085520502501
zeecnla <csrmelchor@gmail.com>
2019-02-25 20:21:28 -0800

    Finished searches and added assertion tests

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -13,22 +13,17 @@ public class BinarySearch {
 		int low = 0;
 		int high = array.length - 1;
 		
-		while(low < high){
+		while(low <= high){
 			
-			int middle = low + (high - low)/2;
-			System.out.println(middle);
-			
+			int middle = low + ((high - low)/2);
 			if(array[middle] == num){
 				return true;
 			}else if(array[middle] > num){
-				low = middle+1;
-				return false;
-			}else if(array[middle] < num){
 				high = middle-1;
+			}else if(array[middle] < num){
+				low = middle+1;
 			}
 		}
 		return false;
 	}
-	
-
 }
--- src/search/SearchMain.java
+++ src/search/SearchMain.java
@@ -1,17 +1,29 @@
 package search;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class SearchMain {
 
 	public static void main(String[] args) {
+		testBinarySearch();
+		testLinearSearch();
 		
-		int [] a = {1,5,12,8,9,20};
-		new LinearSearch();
-		System.out.println( LinearSearch.search(a, 19));
-		
-		int [] b = {1,4,5,9,12};
-		new BinarySearch();
-		System.out.println( BinarySearch.search(b, 9));
 
 	}
-
+	
+	@Test
+	public static void testBinarySearch(){
+		int [] b = {1,2,3,4,5,9,12,21,100,111,123,145};
+		new BinarySearch();
+		assertEquals(BinarySearch.search(b, 111), true);
+	}
+	@Test
+	public static void testLinearSearch(){
+		int [] a = {1,5,12,8,9,20};
+		new LinearSearch();
+		assertEquals(LinearSearch.search(a, 19), false);
+		assertEquals(LinearSearch.search(a, 9), true);
+	}
 }
